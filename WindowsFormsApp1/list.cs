a4da5e65c472208914a7280d4f4e8ec24e5f9811
Джабраил <Djbrail22>
2026-04-25 13:20:39 +0300

    Отправка работы

--- WindowsFormsApp1/list.cs
+++ WindowsFormsApp1/list.cs
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class list : Form
     {
-        public Form1()
+        public list()
         {
             InitializeComponent();
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
